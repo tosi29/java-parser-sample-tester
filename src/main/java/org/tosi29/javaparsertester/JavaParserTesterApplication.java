a177a2e7b6368e0be43f7d0d7d54caf97692bb5f
tosi29 <tosi16@gmail.com>
2020-11-19 22:50:30 +0900

    Initial commit

---
+++ src/main/java/org/tosi29/javaparsertester/JavaParserTesterApplication.java
@@ -0,0 +1,13 @@
+package org.tosi29.javaparsertester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaParserTesterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaParserTesterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tosi29/javaparsertester/JavaParserTesterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tosi29.javaparsertester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaParserTesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
